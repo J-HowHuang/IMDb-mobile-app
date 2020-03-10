7481393614905ad2988d64d2e10e5d1a39bd6f2e
J-How <jhow@J-HowdeMacBook-Pro.local>
2020-03-10 11:12:35 +0800

    create dummy home page and login page

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './route.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -10,7 +12,30 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Center(child: Text('IMDb')),
+      home: TempIndexPage(),
+      routes: route,
+    );
+  }
+}
+
+class TempIndexPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              onPressed: () => Navigator.of(context).pushNamed('/login'),
+              child: Text('Login Page'),
+            ),
+            RaisedButton(
+              onPressed: () => Navigator.of(context).pushNamed('/home'),
+              child: Text('Home Page'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ app/lib/route.dart
@@ -0,0 +1,7 @@
+import './pages/login_page.dart';
+import './pages/home_page.dart';
+
+final route = {
+  '/login': (_) => LoginPage(),
+  '/home': (_) => HomePage(),
+};
